74a9423ae0b6e594b4c836b75d3d274cdaa216f2
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2023-06-24 13:41:23 -0400

    add chatgpt support

--- src/pages/PostConsole.tsx
+++ src/pages/PostConsole.tsx
@@ -1,77 +1,89 @@
+import React, { useState } from 'react';
+import { ChakraProvider, Container, Flex, Text, Input, Button, Textarea, Grid, Image } from '@chakra-ui/react';
+import { TriangleUpIcon } from '@chakra-ui/icons';
+import axios from 'axios';
 
-import React from 'react'
-import {
-  ChakraProvider,
-  Stack,
-  Avatar,
-  AvatarBadge,
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  AlertDescription,
-  FormLabel,
-  Input,
-  FormHelperText,
-  FormErrorMessage,
-  Switch,
-  InputGroup,
-  InputRightElement,
-  Icon,
-  Container,
-  Flex,
-  Text,
-  Button,
-  Textarea,
-  Grid,
-  Image
-} from '@chakra-ui/react'
-import { EmailIcon, TriangleUpIcon } from '@chakra-ui/icons'
+export const PostConsole = () => {
+  const [prompt1, setPrompt1] = useState<string>('');
+  const [prompt2, setPrompt2] = useState<string>('');
+  const [codeSnippet, setCodeSnippet] = useState<string | null>(null);
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
+    try {
+      const generatedCode = await generateCode(prompt1);
+      setCodeSnippet(generatedCode);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
-export const PostConsole = () => 
-<ChakraProvider resetCSS>
-    <Container minWidth={800}>
-      <Flex flexDirection="column" justifyContent="space-around">
-        <Flex justifyContent="flex-end" mb={4}>
-          <Text>World ID </Text>
-        </Flex>
-        <Flex justifyContent="space-between" mb={4}>
-          <Flex>
-            <Text mr={4}>Prompt 1: </Text>
-            <Input />
+  const generateCode = async (prompt: string): Promise<string> => {
+    const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-003/completions';
+    const apiKey = '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';
+
+    const response = await axios.post(
+      apiUrl,
+      {
+        prompt: prompt,
+        max_tokens: 100,
+        n: 1,
+        stop: null,
+        temperature: 0.7
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`
+        }
+      }
+    );
+
+    const codeSnippet = response.data.choices[0].text.trim();
+    return codeSnippet;
+  };
+
+  return (
+    <ChakraProvider>
+      <Container minWidth={800}>
+        <Flex flexDirection="column" justifyContent="space-around">
+          <Flex justifyContent="flex-end" mb={4}>
+            <Text>World ID </Text>
           </Flex>
-          <Flex>
-            <Button variant="solid" size="md">
-              Send
-            </Button>
-          </Flex>
-        </Flex>
-        <Flex mb={4}>
-          <Flex mr={4}>
-            <Text>Prompt 2:</Text>
-          </Flex>
-          <Textarea />
-        </Flex>
-        <Flex alignItems="center">
-          <Grid templateColumns="1fr 5fr 1fr 1fr" gap={3}>
-            <Image height={30} width={30} htmlHeight={30} htmlWidth={30} />
-            <Text>
-              Hello Dogma - Unexpected creation journey of a Design Lab.
-            </Text>
-            <Text>/Pak</Text>
+          <Flex justifyContent="space-between" mb={4}>
             <Flex>
-              <Text>897</Text>
-              <TriangleUpIcon />
+              <Text mr={4}>Prompt: </Text>
+              <Input value={prompt1} onChange={(e) => setPrompt1(e.target.value)} />
             </Flex>
-          </Grid>
-        </Flex>
-      </Flex>
-    </Container>
-  </ChakraProvider>
+            <Flex>
+              <Button variant="solid" size="md" onClick={handleSubmit}>
+                Send
+              </Button>
+            </Flex>
+          </Flex>
+          <Flex mb={4}>
+            <Flex mr={4}>
+              <Text>Body:</Text>
+            </Flex>
+            <Textarea value={prompt2} onChange={(e) => setPrompt2(e.target.value)} placeholder={codeSnippet ?? ""} />
+          </Flex>
 
-//   <div>
-//     <h2>Hello from page A</h2>
-//     <br />
-//     <Link to="/pageB">Navigate to Page B</Link>
-//   </div>
+          <Flex alignItems="center">
+            <Grid templateColumns="1fr 5fr 1fr 1fr" gap={3}>
+              <Image height={30} width={30} htmlHeight={30} htmlWidth={30} />
+              <Text>
+                Hello Dogma - Unexpected creation journey of a Design Lab.
+              </Text>
+              <Text>/Pak</Text>
+              <Flex>
+                <Text>897</Text>
+                <TriangleUpIcon />
+              </Flex>
+            </Grid>
+          </Flex>
+        </Flex>
+      </Container>
+    </ChakraProvider>
+  );
+};
